0fe0b6d087606937c2bc9aaa175578b2e389b86a
Bakhir A.D <Bakhir.A.D.st@gmail.com>
2020-10-22 22:35:33 +0300

    v2 & v3 moved to Core

---
+++ src/Core.py
@@ -0,0 +1,93 @@
+from __future__ import annotations
+from PyQt5 import QtCore, QtGui, QtWidgets, Qt
+from typing import Literal, Tuple, List
+from itertools import chain
+import numpy as np
+from numpy.matrixlib import matrix
+from math import sin, cos, radians
+
+
+class v2:
+    def __init__(self, vec: Tuple[int, int]):
+        self.m = point_2d(*vec)
+
+    def to_QPoint(self) -> QtCore.QPoint:
+        x, y = list(chain(*self.m.tolist()))
+        return QtCore.QPoint(x, y)
+
+
+class v3:
+    def __init__(self, vec: Tuple[int, int, int]):
+        self.m = point_3d(*vec)
+
+    def __str__(self) -> str:
+        tmp = ','.join([str(x) for x in list(chain(*self.m.tolist()))])
+        return f"({tmp})"
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+    def to_v2(self) -> v2:
+        return v2(tuple(chain(*self.m[0:2].tolist())))
+
+    def __add__(self, other: v3) -> v3:
+        m: matrix = self.m + other.m
+        return v3(tuple(chain(*m.tolist())))
+
+    def __mul__(self, sec: matrix):
+        m: matrix = self.m * sec
+        return m
+
+    def x_rotate(self, angle: int):
+        a = radians(angle)
+        tmp = matrix([
+            [1,      0,       0],
+            [0, cos(a), -sin(a)],
+            [0, sin(a),  cos(a)]
+        ])
+        tmp = np.linalg.inv(tmp)
+        self.m = tmp * self.m
+
+    def y_rotate(self, angle: int):
+        a = radians(angle)
+        tmp = matrix([
+            [ cos(a), 0, sin(a)],
+            [      0, 1,      0],
+            [-sin(a), 0, cos(a)]
+        ])
+        tmp = np.linalg.inv(tmp)
+        self.m = tmp * self.m
+
+    def z_rotate(self, angle: int):
+        a = radians(angle)
+        tmp = matrix([
+            [cos(a), -sin(a), 0],
+            [sin(a),  cos(a), 0],
+            [     0,       0, 1]
+        ])
+        tmp = np.linalg.inv(tmp)
+        self.m = tmp * self.m
+
+    def rotate(self, rX: int, rY: int, rZ: int):
+        self.x_rotate(rX)
+        self.y_rotate(rY)
+        self.z_rotate(rZ)
+
+
+def point_3d(x: int, y: int, z: int) -> matrix:
+    return np.matrix(
+        [
+            [x],
+            [y],
+            [z]
+        ]
+    )
+
+
+def point_2d(x: int, y: int) -> matrix:
+    return np.matrix(
+        [
+            [x],
+            [y]
+        ]
+    )
--- src/Drawable.py
+++ src/Drawable.py
@@ -1,94 +1,8 @@
 from PyQt5 import QtCore, QtGui, QtWidgets, Qt
-from typing import Tuple, List
-import numpy as np
-from numpy.matrixlib import matrix
-from itertools import chain
-from math import sin, cos, radians
-
-
-class v2:
-    def __init__(self, vec: Tuple[int, int]):
-        self.m = point_2d(*vec)
-
-    def to_QPoint(self) -> QtCore.QPoint:
-        x, y = list(chain(*self.m.tolist()))
-        return QtCore.QPoint(x, y)
-
-
-class v3:
-    def __init__(self, vec: Tuple[int, int, int]):
-        self.m = point_3d(*vec)
-
-    def __str__(self) -> str:
-        tmp = ','.join([str(x) for x in list(chain(*self.m.tolist()))])
-        return f"({tmp})"
-
-    def __repr__(self) -> str:
-        return self.__str__()
-
-    def to_v2(self) -> v2:
-        return v2(tuple(chain(*self.m[0:2].tolist())))
-
-    def __add__(self, vec: Tuple[int, int, int]):
-        m: matrix = self.m + point_3d(*vec)
-        return v3(tuple(chain(*m.tolist())))
-
-    def __mul__(self, sec: matrix):
-        m: matrix = self.m * sec
-        # assert m.count() == 3
-        # return v3(tuple(chain(*m.tolist())))
-        return m
-
-    def x_rotate(self, angle: int):
-        a = radians(angle)
-        tmp = matrix([
-            [1,      0,       0],
-            [0, cos(a), -sin(a)],
-            [0, sin(a),  cos(a)]
-        ])
-        self.m = tmp * self.m
-
-    def y_rotate(self, angle: int):
-        a = radians(angle)
-        tmp = matrix([
-            [ cos(a), 0, sin(a)],
-            [      0, 1,      0],
-            [-sin(a), 0, cos(a)]
-        ])
-        self.m = tmp * self.m
-
-    def z_rotate(self, angle: int):
-        a = radians(angle)
-        tmp = matrix([
-            [cos(a), -sin(a), 0],
-            [sin(a),  cos(a), 0],
-            [     0,       0, 1]
-        ])
-        self.m = tmp * self.m
-
-    def rotate(self, rX: int, rY: int, rZ: int):
-        self.x_rotate(rX)
-        self.y_rotate(rY)
-        self.z_rotate(rZ)
-
-
-def point_3d(x: int, y: int, z: int) -> matrix:
-    return np.matrix(
-        [
-            [x],
-            [y],
-            [z]
-        ]
-    )
-
-
-def point_2d(x: int, y: int) -> matrix:
-    return np.matrix(
-        [
-            [x],
-            [y]
-        ]
-    )
+from PyQt5.QtCore import QPoint
+from typing import Literal, Tuple, List
+from src.Core import v2, v3
+from enum import Enum
 
 
 class Cube:
@@ -142,8 +56,8 @@ class Cube:
         ret = []
         for connection in self.conn:
             a, b = connection
-            A = (self.points[a-1] + self.center).to_v2().to_QPoint()
-            B = (self.points[b-1] + self.center).to_v2().to_QPoint()
+            A = (self.points[a-1] + v3(self.center)).to_v2().to_QPoint()
+            B = (self.points[b-1] + v3(self.center)).to_v2().to_QPoint()
             ret.append(QtCore.QLine(A, B))
         return ret
 
